59fa67d4d901d6279820f0f2ea999cd882cf668b
Sourabh <sourabhkanwade10@gmail.com>
2019-10-20 18:03:25 +0530

    Added map fragment marker on map added

--- app/src/main/java/com/sourabh/firebasedataretrieve/MainActivity.java
+++ app/src/main/java/com/sourabh/firebasedataretrieve/MainActivity.java
@@ -2,49 +2,64 @@ package com.sourabh.firebasedataretrieve;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback  {
     TextView mEditText;
-    Button mButton;
     DatabaseReference mReference;
     LinkedList<GPSData> mGPSDataList;
+    GoogleMap mMap;
+    double lat;
+    double lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEditText=findViewById(R.id.mTextView);
-        mButton=findViewById(R.id.button);
         mReference= FirebaseDatabase.getInstance().getReference("object");
         mGPSDataList=new LinkedList<>();
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_activity);
+        mapFragment.getMapAsync( this);
+
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
+
         mReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-               // String lat=dataSnapshot.child("latitude").getValue().toString();
-               // String lon=dataSnapshot.child("longitude").getValue().toString();
                 GPSData gpsData=new GPSData();
                 for (DataSnapshot gpsSnapShot: dataSnapshot.getChildren())
                     gpsData =gpsSnapShot.getValue(GPSData.class);
                 mGPSDataList.add(gpsData);
-                mEditText.setText("Done "+mGPSDataList.getLast().latitude);
+                lat= Double.parseDouble(mGPSDataList.getLast().latitude);
+                lon= Double.parseDouble(mGPSDataList.getLast().longitude);
+                mEditText.setText("latitude "+lat+"\n longitude:"+lon);
+
+                LatLng x = new LatLng(lat,lon);
+               // mEditText.append("\nShit:  lat"+lat);
+                mMap.addMarker(new MarkerOptions().position(x).title("Marker in Sydney"));
             }
 
             @Override
@@ -52,5 +67,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera (16.852398,74.581474);
+        //LatLng sydney = new LatLng(lat,lon);
+        //mEditText.append("\nShit:  lat"+lat);
+       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
     }
 }
